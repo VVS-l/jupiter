655c90ef457b5462c90dbe07b9539a1795fc5ff5
VVS-l <97415302+VVS-l@users.noreply.github.com>
2024-08-17 19:39:48 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -111,7 +111,6 @@ function openGameInCloakedTab(url) {
                         if (newTab) {
                             newTab.document.open();
                             newTab.document.write(`
-                                <html lang="en">
                                 <head>
                                     <meta charset="UTF-8">
                                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
