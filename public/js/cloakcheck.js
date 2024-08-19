a9024e18de857cfec014e81286ba9a57c8f9d584
VVS-l <97415302+VVS-l@users.noreply.github.com>
2024-08-18 19:26:52 -0500

    Update cloakcheck.js

--- public/js/cloakcheck.js
+++ public/js/cloakcheck.js
@@ -1,15 +1,17 @@
 // Function to check if the text "Site was launched with launcher" exists anywhere in the page's HTML
 function checkForLaunchMessageInHTML() {
   setTimeout(function() {
+    // Get the entire HTML content of the page
     var htmlContent = document.documentElement.outerHTML;
 
-    if (htmlContent.includes("Site was launched with launcher")) {
+    // Check if the specific text exists in the HTML content
+    if (htmlContent.indexOf("Site was launched with launcher") !== -1) {
       console.log('The site was launched with the launcher.');
     } else {
       console.error('Error: The site was not launched with the launcher. Missing launch message.');
     }
-  }, 2000); // 2000 milliseconds = 2 seconds
+  }, 4000); // 4000 milliseconds = 4 seconds
 }
 
-// Execute the function to check for the launch message in the HTML after a 2-second delay
+// Execute the function to check for the launch message in the HTML after a 4-second delay
 checkForLaunchMessageInHTML();
