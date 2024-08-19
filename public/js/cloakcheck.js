7a8fe9374f1da83ead7889f8cf7489589e1b6c46
VVS-l <97415302+VVS-l@users.noreply.github.com>
2024-08-18 19:33:17 -0500

    Update cloakcheck.js

--- public/js/cloakcheck.js
+++ public/js/cloakcheck.js
@@ -1,13 +1,5 @@
-function checkForLaunchMessageInTextContent() {
-  setTimeout(function() {
-    var textContent = document.body.textContent || document.body.innerText;
-
-    if (textContent.includes("Site was launched with launcher")) {
-      console.log('The site was launched with the launcher.');
-    } else {
-      console.error('Error: The site was not launched with the launcher. Missing launch message.');
-    }
-  }, 2000); // Adjust the delay if necessary
+if (document.body.innerText.includes("Site was launched with launcher")) {
+    console.log("Message found: Site was launched with launcher");
+} else {
+    console.error("Error: Message 'Site was launched with launcher' is missing.");
 }
-
-checkForLaunchMessageInTextContent();
